429e85200d84bf6fe4d1a00f36606636df1a5a5a
dnoberon <johnw.darrington@gmail.com>
2019-09-23 20:17:28 -0600

    added value pair methods

--- graphson.go
+++ graphson.go
@@ -14,8 +14,11 @@
 
 // Package graphson provides a parser for GraphSON structured json data. This interface is primarily consumed by the
 // gremlin go language variant. It provides a GraphSON version agnostic interface to the user, though only GraphSON 3.0+ is
-// currently covered. Visit http://tinkerpop.apache.org/docs/3.4.2/dev/io/#graphson-3d0 for more information
+// currently covered. Visit http://tinkerpop.apache.org/docs/3.4.2/dev/io/#graphson-3d0 for more information. This is not
+// intended to handle things like
 package graphson
+
+import "time"
 
 // ValueType represents the GraphSON equivalent type of a value in a ValuePair type.
 type ValueType int
@@ -91,4 +94,115 @@ type Property struct {
 type ValuePair struct {
 	Type  ValueType
 	Value interface{}
+}
+
+func (vp ValuePair) AsVertex() VertexRecord {
+	if vp.Type != Vertex {
+		return VertexRecord{}
+	}
+
+	return vp.Value.(VertexRecord)
+}
+
+func (vp ValuePair) AsVertexProperty() VertexPropertyRecord {
+	if vp.Type != VertexProperty {
+		return VertexPropertyRecord{}
+	}
+
+	return vp.Value.(VertexPropertyRecord)
+}
+
+func (vp ValuePair) AsEdge() EdgeRecord {
+	if vp.Type != Edge {
+		return EdgeRecord{}
+	}
+
+	return vp.Value.(EdgeRecord)
+}
+
+func (vp ValuePair) AsProperty() Property {
+	if vp.Type != EdgeProperty {
+		return Property{}
+	}
+
+	return vp.Value.(Property)
+}
+
+func (vp ValuePair) AsSet() []ValuePair {
+	if vp.Type != Set {
+		return nil
+	}
+
+	return vp.Value.([]ValuePair)
+}
+
+func (vp ValuePair) AsMap() map[ValuePair]ValuePair {
+	if vp.Type != Map {
+		return nil
+	}
+
+	return vp.Value.(map[ValuePair]ValuePair)
+}
+
+func (vp ValuePair) AsString() string {
+	if vp.Type != String {
+		return ""
+	}
+
+	return vp.Value.(string)
+}
+
+func (vp ValuePair) AsInt32() int {
+	if vp.Type != Int32 {
+		return 0
+	}
+
+	return vp.Value.(int)
+}
+
+func (vp ValuePair) AsInt64() int64 {
+	if vp.Type != Int64 {
+		return 0
+	}
+
+	return vp.Value.(int64)
+}
+
+func (vp ValuePair) AsFloat32() float32 {
+	if vp.Type != Double {
+		return 0
+	}
+
+	return vp.Value.(float32)
+}
+
+func (vp ValuePair) AsFloat64() float64 {
+	if vp.Type != Float {
+		return 0
+	}
+
+	return vp.Value.(float64)
+}
+func (vp ValuePair) AsTime() time.Time {
+	if vp.Type != Timestamp {
+		return time.Time{}
+	}
+
+	return vp.Value.(time.Time)
+}
+
+func (vp ValuePair) AsClAss() string {
+	if vp.Type != Class {
+		return ""
+	}
+
+	return vp.Value.(string)
+}
+
+func (vp ValuePair) AsUUID() string {
+	if vp.Type != UUID {
+		return ""
+	}
+
+	return vp.Value.(string)
 }
